c953c079bacc1483ee7b09623c3ab09cfd4b7508
Sergio19991 <alum.sbejaranoa.2020@iesalixar.org>
2020-12-06 12:06:42 +0100

    Primer commit para la Trea de Eclipse y Git.

---
+++ eclipse-workspace/tareaEclipseYGit/src/tareaEclipseYGit/suma.java
@@ -0,0 +1,18 @@
+package tareaEclipseYGit;
+
+import java.util.Scanner;
+
+public class suma {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        int num1, num2;
+
+	        System.out.println("Introduce el Primer Número:");
+	        num1 = sc.nextInt();
+	        System.out.println("Introduce el Segundo Número:");
+	        num2 = sc.nextInt();
+
+	        System.out.println("El Resultado, es " + (num1 + num2));
+	        
+	    }
+}
