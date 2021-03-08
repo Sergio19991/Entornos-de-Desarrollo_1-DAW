cbabda0d4d60dec577657d8429e2d5d485b721c3
Sergio19991 <alum.sbejaranoa.2020@iesalixar.org>
2021-03-08 13:29:13 +0100

    JUnit

--- ECLIPSE/BancoJUnit/BancoJUnit/test/banco/CuentaTest.java
+++ ECLIPSE/BancoJUnit/BancoJUnit/test/banco/CuentaTest.java
@@ -8,29 +8,15 @@ class CuentaTest {
 
 	@Test
 	void testIngresarDinero() {
-		Cuenta cuenta1 = new Cuenta("321465978", "TITULAR 1");
-		Cuenta cuenta2 = new Cuenta("465798321", "TITULAR 2");
+		Cuenta c = new Cuenta("100", "Veigar");
+		double saldoInicial = c.getSaldo();
+		c.ingresarDinero(50);
+		assertEquals(saldoInicial+50, c.getSaldo(), 0.01);
+	}
 
-		cuenta1.ingresarDinero(26.74);
-		equals(26.74);
-		
-		cuenta2.ingresarDinero(-12.74);
-		equals("El dinero ingresado debe ser mayor que 0");
-	}
-	
-	@Test
-	void retirarDinero() {
-		Cuenta cuenta1 = new Cuenta("312455587", "TITULAR 1");
-		Cuenta cuenta2 = new Cuenta("984744552", "TITULAR 2");
-		
-		cuenta1.ingresarDinero(125.50);
-		cuenta1.retirarDinero(25.50);
-		equals(25.50);
-		
-		cuenta2.ingresarDinero(100.25);
-		cuenta2.retirarDinero(100.25);
-		cuenta2.retirarDinero(25.50);
-		cuenta2.bloquearCuenta();
-		equals(true);
-	}
+	/*@Test
+	void testRetirarDinero() {
+		fail("Not yet implemented");
+	}*/
+
 }
